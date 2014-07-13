cdcee7c07976c5e7cd2acc6587745f136c6ffb06
MichaelArestad <marestad@gmail.com>
2014-07-12 20:07:06 -0600

    Boom.

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+	// 1. All configuration goes here
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				options: {
+					// Can be nested, compact, compressed, expanded
+					style: 'compact',
+					sourcemap: true
+				},
+				files: {
+					'css/sasstronauts.css': 'scss/sasstronauts.scss'
+				}
+			}
+		},
+		autoprefixer: {
+			options: {
+				// Task-specific options go here.
+			},
+			global: {
+				options: {
+					// Target-specific options go here.
+					// browser-specific info: https://github.com/ai/autoprefixer#browsers
+					// DEFAULT: browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1']
+					browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1', 'ie 8', 'ie 9'],
+					map: true
+				},
+				src: 'css/*.css'
+			},
+		},
+		watch: {
+			css: {
+				files: ['scss/*.scss', 'scss/**/*.scss'],
+				tasks: ['sass', 'autoprefixer'],
+				options: {
+					livereload: true,
+					spawn: false,
+				}
+			}
+		}
+	});
+
+	// 3. Where we tell Grunt we plan to use this plug-in.
+	grunt.loadNpmTasks('grunt-contrib-watch');      // watch files for changes
+	grunt.loadNpmTasks('grunt-contrib-sass');       // Gettin Sassy!
+	grunt.loadNpmTasks('grunt-autoprefixer');       // Auto-freaking-prefixer!!!
+
+	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask('default', ['watch']);
+
+};
