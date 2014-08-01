a373ab7b38ca7f235ff5e855191afd57d3fcc2c1
Chris Holder <chris@see8ch.com>
2014-08-01 01:24:49 -0600

    pre max height/scroll, styled file structure slide

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ function resizeDiv() {
   vpw = $(window).width();
   vph = $(window).height();
 
+
+  // Code
+  $('pre').css({'max-height': vph + 'px'});
+
+  // Demos
   $('.demo').css({'width': vpw + 'px'});
   $('.demo').css({'height': vph + 'px'});
   $('.demo pre').css({'height': vph + 'px'});
